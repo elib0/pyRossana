5a91440ffb03a1edd239d0743084c1545154fa60
Elibo <elijose.c@gmail.com>
2013-10-14 21:28:30 -0430

    Estructura

---
+++ pyRossana/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^|home$', 'pyRossana.views.home', name='home'),
+    # url(r'^pyRossana/', include('pyRossana.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
