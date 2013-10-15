252bbfb09eccfb84eb60f519d4c38bf57795ba90
Eli J. Chavez <skat0@hotmail.com>
2013-10-15 15:17:02 -0430

    Pequeños cambios

--- pyRossana/urls.py
+++ pyRossana/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^|home$', 'pyRossana.views.home', name='home'),
+    url(r'^$', 'pyRossana.views.home', name='home'),
     # url(r'^pyRossana/', include('pyRossana.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
