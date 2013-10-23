37c5c0e53c5bf08bcc009b3c28e95db2ffbe0979
Elito <elijose.c@gmail.com>
2013-10-22 23:46:45 -0430

    Shopping CArt

--- pyRossana/views.py
+++ pyRossana/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from persons import forms
+from store import models
 
 
 def home(request):
-    form1 = forms.PersonForm()
-    form2 = forms.PromoterForm()
-    return render(request, 'index.html', {'form1': form1, 'form2': form2})
+    # form1 = forms.PersonForm()
+    # form2 = forms.PromoterForm()
+    products = models.Product.objects.all()
+    if request.user.is_authenticated():
+        num = len(request.user.cart_set.filter(status=models.STATUS_CHOICES[0]))
+    else:
+        num = 0
+    return render(request, 'index.html', {'products': products, 'num': num})
