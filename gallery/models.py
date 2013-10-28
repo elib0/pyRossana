8c8ae6cd5cb1d98c1d7a39d28c08ecbd836bb477
Elibo <elijose.c@gmail.com>
2013-10-27 20:00:29 -0430

    Added Imagen Gallery APP

---
+++ gallery/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Folder(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField('Nombre Carpeta', max_length=20)
+    last_modification = models.DateField('Ultima modificación',
+                                         auto_now=True,
+                                         auto_now_add=True)
+
+
+class Picture(models.Model):
+    def __str__(self):
+        return self.title
+    title = models.CharField('Titulo', max_length=15)
+    picture = models.ImageField('Foto')
+    folder = models.ForeignKey(Folder)
+
+
+class Comment(models.Model):
+    picture = models.ForeignKey(Picture)
+    text = models.TextField("comentario")
+    date = models.DateField("Fecha de publicación")
