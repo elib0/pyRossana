ce98d416c46cadd64da85d0c2bb55cbad5177479
Elibo <elijose.c@gmail.com>
2013-10-29 19:40:48 -0430

    Added font-face, Fixes in views, Started ui design

--- pyRossana/views.py
+++ pyRossana/views.py
@@ -8,9 +8,10 @@ def home(request):
     num = 0
     form = forms.PromoterForm()
     products = models.Product.objects.all()
+    categories = models.Categorie.objects.all()
     if request.user.is_authenticated():
         num = len(request.user.cart_set.filter(status=models.STATUS_CHOICES[0]))
     return render(request,
                   'index.html',
-                  {'products': products, 'num': num, 'form': form},
+                  {'categories': categories, 'products': products, 'num': num, 'form': form},
                   context_instance=RequestContext(request))
